6b37ed774a082802d79e3fef01bb24fea57ca006
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-02 23:36:04 -0500

    Se prepara el ambiente, se crea la capeta img y se agrega el LogoJSBM.png, igualmente se crea la carpeta componentes para ir agregando los componentes necesarios durante el desarrollo del proyecto.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import jsbmLogo from './img/LogoJSBM.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-contenedor">
+        <img
+          className="logo"
+          src={jsbmLogo}
+          alt="Logo" />
+      </div>
     </div>
   );
 }
