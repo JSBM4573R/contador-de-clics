8d24abb65cb87a2dee5c0daa421cd2bb392cee37
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-02 23:39:15 -0500

    Se genera los comentarios respectivos para la importacion del logo.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//Se genera la importacion del logo primero definiendo un nombre y luego su carpeta de origen.
 import jsbmLogo from './img/LogoJSBM.png';
 
 function App() {
@@ -7,8 +8,13 @@ function App() {
       <div className="logo-contenedor">
         <img
           className="logo"
+          //Dentro de corchetes se inserta el logo importado anteriormente
+          //bajo el mismo nombre declarado. En este caso jsbmLogo
           src={jsbmLogo}
           alt="Logo" />
+      </div>
+      <div className="contenedor-principal">
+
       </div>
     </div>
   );
